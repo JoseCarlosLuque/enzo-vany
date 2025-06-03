6bab994738cde7565ccdcefd901476ae86f8fda7
José Carlos Luque Castro <luquecastrojosecarlos@gmail.com>
2025-06-03 15:59:50 +0200

    Arreglo un fallo en el panel admin

--- Enzo_vani_app/src/App.tsx
+++ Enzo_vani_app/src/App.tsx
@@ -1,13 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Cart from "./pages/cart";
 import Login from "./pages/login";
-import AdminPanel from "./pages/admin"
-const role = localStorage.getItem("role");
+import AdminPanel from "./pages/admin";
 
 const App: React.FC = () => {
+
+   const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    setRole(localStorage.getItem("role"));
+  }, []);
+
   return (
     <Router>
       <nav className="p-4 bg-gray-800 text-white flex justify-between items-center">
@@ -15,16 +22,15 @@ const App: React.FC = () => {
           <Link to="/">Home</Link>
           <Link to="/about">Acerca de nosotros</Link>
           <Link to="/contact">Contacto</Link>
-
         </div>
         <div className="space-x-4">
           {role === "admin" && (
             <Link to="/admin" className="hover:underline">
-              🛠 Panel Admin
+              🛠️ Panel Admin
             </Link>
           )}
           <Link to="/login" className="hover:underline">
-            🔑 Login
+           🔑 Login
           </Link>
           <Link to="/cart" className="hover:underline">
             🛒 Carrito
@@ -36,9 +42,9 @@ const App: React.FC = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login/>} /> 
       </Routes>
     </Router>
   );
